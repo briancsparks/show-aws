a6fb33725806e3788d03a02fcb11e30ebd204e45
Brian C Sparks <briancsparks@gmail.com>
2022-11-12 15:58:37 -0800

    refactoring

--- libs/aws-json.js
+++ libs/aws-json.js
@@ -28,17 +28,6 @@ function autoCleanAwsJson(json) {
     }
   }
 
-  // const result = {...orig,
-  //   awsOrig: {
-  //     ...(orig.awsOrig || {}),
-  //     ...data.awsOrig,
-  //   },
-  //   data: {
-  //     ...(orig.data || {}),
-  //     ...data.data,
-  //   }
-  // };
-
   return data;
 }
 
@@ -49,6 +38,83 @@ function autoCleanAwsJson(json) {
  * @returns {*[]}
  */
 function objKeyArray(obj, extra ={}) {
+  let result = [];
+  for (let key in obj) {
+    if (key === 'Reservations') {
+      return objKeyArrayReservations(obj, extra);
+    }
+
+    if (key[0] === '$') { continue; }
+    if (key === 'NextToken') { continue; }
+    if (!Array.isArray(obj[key])) { continue; }
+    // if (obj[key].length === 0) { continue; }
+
+    let   level1Values = obj[key];
+
+    const root   = key.substring(0, key.length-1);            /* key: 'Vpcs', root: 'Vpc' */
+    let   idKey  = idKeyFromType(key, root + 'Id');      /* idKey: 'VpcId' */
+    let   map    = {};
+    let   values = [...level1Values];
+
+    // if (key === 'Reservations') {
+    //   values = [];
+    // }
+
+    if (Array.isArray(level1Values)) {
+      for (let value of level1Values) {
+        value = {...value, ...extra};
+
+        let itemKey = value[idKey];
+        itemKey = itemKey || value[idKey = (key + 'Id')];
+        itemKey = itemKey || value[idKey = (root + 'Name')];
+        itemKey = itemKey || value[idKey = (key + 'Name')];
+        itemKey = itemKey || value[idKey = idKeyFromType(key)];
+        itemKey = itemKey || value[idKey = ('Name')];
+        itemKey = itemKey || value[idKey = ('Key')];
+        itemKey = itemKey || value[idKey = (root)];
+
+        errIf(!itemKey, `Cannot determine key`, {root, idKey, item: value});
+
+        // // Special processing for instances -- value is the list of reservations: {Groups:[], Instances:[], ...}
+        // if (key === 'Reservations') {
+        //
+        //   const reservation = {...value};
+        //   const {OwnerId, RequesterId, ReservationId} = reservation;
+        //   const subItems = objKeyArray(reservation, {OwnerId, RequesterId, ReservationId});
+        //   for (let subItem of subItems) {
+        //     const [subKey, subKeyId, oneInstancesMap, oneInstancesAwsValues] = subItem;     /* 'Instances', 'InstanceId', {'i-abc123':instanceData}, [Instances array] */
+        //     if (subKey !== 'Instances') {
+        //       continue                                              /* subKey will also be 'Groups', 'OwnerId', 'RequesterId', etc */
+        //     }
+        //
+        //     map = {...map, ...oneInstancesMap};
+        //     values = [...values, ...oneInstancesAwsValues];
+        //   }
+        //
+        // } else {
+          map[itemKey] = {...value};
+        // }
+      }
+
+      // if (key === 'Reservations') {
+      //   key = 'Instances';
+      //   idKey = 'InstanceId';
+      // }
+
+      result.push([key, idKey, map, values]);     /* 'Vpcs', 'VpcId', {'vpc-abc123':vpcData}, [Vpcs array] */
+    }
+  }
+
+  return result;
+}
+
+/** -------------------------------------------------------------------------------------------------------------------
+ *
+ * @param obj
+ * @param extra
+ * @returns {*[]}
+ */
+function objKeyArrayReservations(obj, extra ={}) {
   let result = [];
   for (let key in obj) {
     if (key[0] === '$') { continue; }
@@ -63,9 +129,9 @@ function objKeyArray(obj, extra ={}) {
     let   map    = {};
     let   values = [...level1Values];
 
-    if (key === 'Reservations') {
+    // if (key === 'Reservations') {
       values = [];
-    }
+    // }
 
     if (Array.isArray(level1Values)) {
       for (let value of level1Values) {
@@ -98,15 +164,15 @@ function objKeyArray(obj, extra ={}) {
             values = [...values, ...oneInstancesAwsValues];
           }
 
-        } else {
-          map[itemKey] = {...value};
+        // } else {
+        //   map[itemKey] = {...value};
         }
       }
 
-      if (key === 'Reservations') {
+      // if (key === 'Reservations') {
         key = 'Instances';
         idKey = 'InstanceId';
-      }
+      // }
 
       result.push([key, idKey, map, values]);     /* 'Vpcs', 'VpcId', {'vpc-abc123':vpcData}, [Vpcs array] */
     }
